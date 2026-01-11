39b07e77fc3bdb7f6d129bccf4f31a992a1fa174
omarmatter2 <omarmatter2001@gmail.com>
2026-01-11 18:09:58 +0200

    Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             TestimonialSeeder::class,
             SubscriberSeeder::class,
             SocialMediaSeeder::class,
+            NavItemSeeder::class,
         ]);
     }
 }
